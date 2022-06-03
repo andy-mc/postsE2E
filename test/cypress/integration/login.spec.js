ea22cb1827aceede995d40dc81d131a12a98d34a
Andrés Muñoz <andres.munoz@idukay.com>
2022-06-02 22:34:45 -0500

    create comand createUser

--- test/cypress/integration/login.spec.js
+++ test/cypress/integration/login.spec.js
@@ -12,22 +12,13 @@ describe("Login foooo", () => {
 
   it("should register a user", () => {
     cy.get("@userFixture").then((user) => {
-      cy.contains("Crear una cuenta").click()
-      cy.get("#name").type(user.name)
-      cy.get("#title").type(user.company)
-      cy.get("#email2").type(user.email)
-      cy.get("#password2").type(user.password)
-      cy.contains(".button", "Registrarse").click()
-      cy.wait(3000)
-      cy.get(".error-msg").should('not.exist')
+      cy.createUser(user);
     })
   })
 
   it("should login a user", () => {
     cy.get("@userFixture").then((user) => {
       cy.login(user.email, user.password)
-      cy.contains(".button", "Ingresar").click()
-      cy.wait(3000)
       cy.contains("a", "Dashboard").should('be.visible')
     })
   })
@@ -35,8 +26,6 @@ describe("Login foooo", () => {
   it("should fail login for incorrect user", () => {
     cy.get("@userFixture").then((user) => {
       cy.login(user.email, user.wrong_password)
-      cy.contains(".button", "Ingresar").click()
-      cy.wait(3000)
       cy.get(".error-msg").should('exist')
       cy.get(".error-msg").should('be.visible')
     })
--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -11,10 +11,6 @@
 //
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
-Cypress.Commands.add("login", (email, password) => {
-  cy.get("#email1").type(email)
-  cy.get("#password1").type(password)
-})
 //
 //
 // -- This is a child command --
@@ -27,3 +23,22 @@ Cypress.Commands.add("login", (email, password) => {
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("createUser", (user) => {
+  cy.visit("/login")
+  cy.contains("Crear una cuenta").click()
+  cy.get("#name").type(user.name)
+  cy.get("#title").type(user.company)
+  cy.get("#email2").type(user.email)
+  cy.get("#password2").type(user.password)
+  cy.contains(".button", "Registrarse").click()
+  cy.wait(3000)
+  cy.get(".error-msg").should('not.exist')
+})
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.get("#email1").type(email)
+  cy.get("#password1").type(password)
+  cy.contains(".button", "Ingresar").click()
+  cy.wait(3000)
+})
